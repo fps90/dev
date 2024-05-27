08aa09079763cf1a4d25ec48d8aeb755c84db04b
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-27 15:55:32 +0300

    Update sudoers.py

--- YukkiMusic/plugins/sudo/sudoers.py
+++ YukkiMusic/plugins/sudo/sudoers.py
@@ -108,7 +108,7 @@ async def sudoers_list(client, message: Message, _):
             count += 1
         except Exception:
             continue
-        text += f"{count}➤ {user}\n"
+        text += f"{count}➤ {user} - `{x}`\n"
     smex = 0
     for user_id in SUDOERS:
         if user_id not in OWNER_ID:
@@ -119,7 +119,7 @@ async def sudoers_list(client, message: Message, _):
                     smex += 1
                     text += _["sudo_6"]
                 count += 1
-                text += f"{count}➤ {user}\n"
+                text += f"{count}➤ {user} - `{user_id}`\n"
             except Exception:
                 continue
     if not text:
