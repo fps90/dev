aef4a1b580106b246f17a1d041d1d4d444091f42
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-05-28 15:27:23 +0300

    Create اضفني.py

---
+++ YukkiMusic/plugins/اضفني.py
@@ -0,0 +1,21 @@
+from pyrogram import filters
+from 
+from YukkiMusic.utils.database import get_client
+from YukkiMusic import app
+from YukkiMusic.core.userbot import assistants
+
+@app.on_message(filters.command(["اضفني","add","سجلني"]))
+async def add_contact(client, message):
+    try:
+        for num in assistants:
+            USER = await get_client(num)
+            try:
+                if message.from_user.username:
+                    await USER.add_contact(message.from_user.username, message.from_user.first_name)
+                else:
+                    await USER.add_contact(message.from_user.id, message.from_user.first_name)
+                await message.reply_text("You have been added to the contacts of the assistant's account in Netah ")
+            except Exception as e:
+                await message.reply_text(f"Error : {e}")
+    except Exception as e:
+            await message.reply_text(f"Error : {e}")
